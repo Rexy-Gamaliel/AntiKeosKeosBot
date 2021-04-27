043009e1f363b8abb208cd48cbe3b3052792fa24
Rexy-Gamaliel <rexygaming400@gmail.com>
2021-04-27 13:28:25 +0700

    Update coba2 frontend

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
@@ -17,10 +17,23 @@ class Todo(db.Model):
         return '<Task %r>' %self.id
 
 
-@app.route('/')
-
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        task_content = request.form['content']
+        
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error on adding task"
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
 
 
 if __name__ == "__main__":
