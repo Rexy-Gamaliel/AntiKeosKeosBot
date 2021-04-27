5b762268ad7cc24c56a74c4ccdd3ebddc81c6ba8
Rexy-Gamaliel <rexygaming400@gmail.com>
2021-04-27 23:07:55 +0700

    Frontend lumayan rapih

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ class Todo(db.Model):
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        
+
         new_task = Todo(content=task_content)
 
         try:
@@ -35,6 +35,31 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template("index.html", tasks=tasks)
 
+
+@app.route('/delete/<int:id>')
+def delete(id: int):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Error deleting tasks'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error updateing task'
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
