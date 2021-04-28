b0d4e3195902e90336df7f41898fea32d2f6721e
dethaa <sharonmarbun12@gmail.com>
2021-04-28 22:26:06 +0700

    update file sql

--- test/AntiKeosKeosBot.sql
+++ test/AntiKeosKeosBot.sql
@@ -32,7 +32,7 @@ DROP TABLE IF EXISTS `chats`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `chats` (
   `id` int(11) NOT NULL,
-  `text` varchar(5000) DEFAULT NULL,
+  `text` varchar(1000) DEFAULT NULL,
   `source` varchar(10) DEFAULT NULL,
   `timeStamp` time DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -84,4 +84,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-28 21:19:47
+-- Dump completed on 2021-04-28 22:24:48
